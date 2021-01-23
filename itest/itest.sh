99b6499f7c0f98b7d33f9c57189d8df1c2690214
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-23 12:37:05 -0800

    Destroy the test stack

--- itest/itest.sh
+++ itest/itest.sh
@@ -3,19 +3,14 @@
 test_stack() {
   local stack_dir=$1
   cd $stack_dir
-  # get stackname
   echo "NOW IN: $(pwd)"
   ls ..
   npx ../..
   npx cdk bootstrap
   npx cdk deploy --require-approval never
-  echo "AWS_ACCESS_KEY_ID length:"
-  echo ${AWS_ACCESS_KEY_ID} | wc
-  echo "AWS_DEFAULT_REGION length:"
-  echo ${AWS_DEFAULT_REGION} | wc
+  npx cdk destroy --force
 }
 
-# bootstrap
 
 # test stacks
 for stack_dir in $(ls -d */)
