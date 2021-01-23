fc5b1a69fdad84e5c60abcf276cfd59c90ae1a5b
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-23 08:28:01 -0800

    Use stackname in naming a real stack

--- itest/it01/bin/it01.ts
+++ itest/it01/bin/it01.ts
@@ -2,7 +2,7 @@
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
 import { It01StackA } from "../lib/it01-stack-a";
+const stackname = require("../../../index");
 
 const app = new cdk.App();
-
-new It01StackA(app, "It01StackA");
+new It01StackA(app, "It01StackA" + stackname());
