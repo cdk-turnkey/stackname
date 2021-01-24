34c9c31fc75cf1e41637496fda1d65f3360355a7
Douglas Naphas <douglasnaphas@gmail.com>
2021-01-23 18:03:24 -0800

    Remove redundant CLI tests

--- itest/itest.sh
+++ itest/itest.sh
@@ -23,22 +23,6 @@ test_cli() {
   return 0
 }
 
-OUT1=$(GITHUB_REPOSITORY=cdk-turnkey/stackname GITHUB_REF=refs/heads/abCde npx ..)
-EXPECTED1="Cdk-turnkeyStacknameAbcde"
-if [[ "${OUT1}" != ${EXPECTED1} ]]
-then
-  echo "cli integration test failure: expected ${EXPECTED1}, got ${OUT1}"
-  exit 1
-fi
-
-OUT2=$(GITHUB_REPOSITORY=MyOrg/MyStack GITHUB_REF=refs/heads/main npx .. --suffix StackOne)
-EXPECTED2="MyorgMystackMain-StackOne"
-if [[ "${OUT2}" != ${EXPECTED2} ]]
-then
-  echo "cli integration test failure: expected ${EXPECTED2}, got ${OUT2}"
-  exit 1
-fi
-
 test_cli cdk-turnkey/stackname refs/heads/abCde Cdk-turnkeyStacknameAbcde
 test_cli MyOrg/MyStack refs/heads/main StackOne MyorgMystackMain-StackOne
 
