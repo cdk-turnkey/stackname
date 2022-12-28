ee9f16adbe85dc119fde39d57121f910ceb1097d
Douglas Naphas <douglasnaphas@gmail.com>
2022-12-28 12:58:08 -0500

    Get length without trailing newline
    gh-1
    
    This is the only difference I can find between this file and the length test I’m attempting in itest.sh. Maybe without a newline output gets overwritten?

--- itest/clitest.sh
+++ itest/clitest.sh
@@ -2,5 +2,5 @@
 GITHUB_REPOSITORY=aaa/bbb GITHUB_REF=ccc npx ..
 o1=$(GITHUB_REPOSITORY=ddd/eee GITHUB_REF=fff npx ..)
 echo $o1
-o1_length=$(echo $o1 | wc -c | awk '{print $1}')
+o1_length=$(echo -n $o1 | wc -c | awk '{print $1}')
 echo $o1_length
