961ee03c21fd6a10f16623d0e2216f809ba37f48
Douglas Naphas <douglasnaphas@gmail.com>
2022-12-28 13:40:29 -0500

    Add length test from itest
    gh-1

--- itest/clitest.sh
+++ itest/clitest.sh
@@ -4,3 +4,23 @@ o1=$(GITHUB_REPOSITORY=ddd/eee GITHUB_REF=fff npx ..)
 echo $o1
 o1_length=$(echo -n $o1 | wc -c | awk '{print $1}')
 echo $o1_length
+
+# -h6 should output something like:
+# s-abcde-abc123
+# Check the length
+# prove this test method works
+GITHUB_REPOSITORY=vvv/www GITHUB_REF=rrr npx ..
+output1=$(GITHUB_REPOSITORY=vvv/www GITHUB_REF=rrr npx ..)
+output1_length=$(echo -n $output1 | wc -c | awk '{print $1}')
+echo $output1
+if [[ $output1_length -ne 9 ]]
+then
+  echo "Wrong length"
+  exit 2
+fi
+output_h6=$(GITHUB_REPOSITORY=vvv/www GITHUB_REF=rrr npx .. -h6)
+output_h6_length=$(echo -n $output_h6 | wc -c | awk '{print $1}')
+
+
+
+# Similarly, with a suffix
