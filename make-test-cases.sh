01c70154bc85b85a526cce301c3f3a7573e0d9f2
Douglas Naphas <douglasnaphas@gmail.com>
2022-12-29 10:45:45 -0500

    Encode the main hashing logic in a helper
    gh-1

---
+++ make-test-cases.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+awk 'NR == 116, NR == 120' index.test.ts | tr -d '${"}' |
+while read line
+do
+  suffix=$(echo $line | awk '{print $3}') ;
+  github_repository=$(echo $line | awk '{print $5}') ;
+  github_ref=$(echo $line | awk '{print $7}') ;
+  org_first_letter=$(echo $github_repository | cut -c1) ;
+  repo_first_letter=$(echo $github_ref | cut -f2 -d '/' | cut -c1) ;
+  branch=$(echo $github_ref | awk -v FS='/' '{print $NF}') ;
+  branch_first_three_letters=${branch:0:3} ;
+  sha_github_repository=$(openssl sha256 <(echo -n $github_repository) | awk '{print $2}') ;
+  sha_github_ref=$(openssl sha256 <(echo -n $github_ref) | awk '{print $2}') ;
+  sha=$(openssl sha256 <(echo -n ${sha_github_repository}${sha_github_ref}) | awk '{print $2}') ;
+  stackname=s${org_first_letter}${repo_first_letter}${branch_first_three_letters}-${sha:0:6}-${suffix} ;
+  echo "${line} | ${stackname}" ;
+done
