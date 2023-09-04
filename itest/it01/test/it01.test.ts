36372d2d329cdd42a5aa5ad2314911370607d0ec
Douglas Naphas <douglasnaphas@gmail.com>
2023-09-04 09:29:42 -0400

    Import just App from aws-cdk-lib

--- itest/it01/test/it01.test.ts
+++ itest/it01/test/it01.test.ts
@@ -1,9 +1,9 @@
-import * as cdk from "aws-cdk-lib";
+import { App } from "aws-cdk-lib";
 import * as It01A from "../lib/it01-stack-a";
 
 describe("Stack A", () => {
   test("can instantiate stack", () => {
-    const app = new cdk.App();
+    const app = new App();
     // WHEN
     const stack = new It01A.It01StackA(app, "MyTestStack");
     // THEN
